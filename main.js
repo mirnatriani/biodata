e01f527c8686220b74cc1ee45e8279958dd17594
Mirna Triani <141211528+mirnatriani@users.noreply.github.com>
2023-12-04 13:12:13 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let nama = "Nama: Mirna triani";
+let kelas = "Kelas: XI";
+let jurusan = "Jurusan: PPLG";
+let umur = "Umur: 16";
+let alamat = "Alamat: Kp.gunung salikur";
+
+document.write(nama + "<br>", kelas + "<br>", jurusan + "<br>", umur + "<br>", alamat + "<br>");
